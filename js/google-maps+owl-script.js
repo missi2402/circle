28810773bcab48b04590c803e08097057e58bfae
Valentyna Antoniuk <antoniuk.valentyna@gmail.com>
2016-11-23 15:38:23 +0200

    Changed footer. Added slider.

---
+++ js/google-maps+owl-script.js
@@ -0,0 +1,57 @@
+ jQuery(document).ready(function() {
+    // google maps
+     var myCenter = new google.maps.LatLng(50.000912, 36.251374);
+
+     function initialize() {
+         var mapProp = {
+             scrollwheel: false,
+             center: myCenter,
+             zoom: 17,
+             mapTypeId: google.maps.MapTypeId.ROADMAP
+         };
+         var image = 'img/map.png';
+         var map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
+         var marker = new google.maps.Marker({
+             position: myCenter,
+             icon: image
+         });
+         marker.setMap(map);
+     }
+     google.maps.event.addDomListener(window, 'load', initialize);
+     // slider
+     $(".owl-carousel").owlCarousel({
+         items: 4,
+         loop: true,
+         margin: 20,
+         nav: true,
+         navText: ["<img src='img/circle-left.png'>", "<img src='img/circle-right.png'>"],
+         merge: true,
+         responsiveClass: true,
+         responsive: {
+             0: {
+                 items: 2
+             },
+             700: {
+                 items: 3
+             },
+             1000: {
+                 items: 4
+             }
+         },
+         autoplay: true,
+         autoplayTimeout: 9000,
+         autoplayHoverPause: true,
+         autoplaySpeed: 5000,
+         smartSpeed: 1000
+     });
+     // scroll
+     $('.nav a').click(function(event) {
+      event.preventDefault();
+      var href=$(this).attr('href');
+      var target=$(href);
+      var top=target.offset().top;
+      $('html,body').animate({
+        scrollTop: top
+        }, 1200);   
+      });
+ });
